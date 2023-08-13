04342f60cf06bc88ccc14b10513b2b72b2f6e2bb
eyhdev <alaqsa335@gmail.com>
2023-08-13 16:43:30 +0300

    Initial Commit

---
+++ BlurText/BlurTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlurTextApp.swift
+//  BlurText
+//
+//  Created by eyh.mac on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BlurTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
